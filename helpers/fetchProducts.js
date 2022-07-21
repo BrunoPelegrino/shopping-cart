34c4749e2d859b055ee70434271798616ee74ae2
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2022-07-21 10:39:24 -0300

    commit atualizado

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,7 +2,7 @@ const fetchProducts = async (item) => {
   const url = (`https://api.mercadolibre.com/sites/MLB/search?q=${item}`);
   const computer = await fetch(url)
   .then((result) => result.json())
-  .then((resultJson) => resultJson.results)
+  .then((resultJson) => resultJson.result)
   .catch((error) => error);
   return computer;
 }; 
